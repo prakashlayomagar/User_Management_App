04ebac2380fcbfde0c3e96536426fc0b4aab2d4c
prakash <petermagr4@gmail.com>
2025-01-08 14:07:46 -0500

    Routes Added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './Pages/layout/layout.component';
+import { UserListComponent } from './Pages/user-list/user-list.component';
+import { LoginComponent } from './Pages/login/login.component';
+import { CreateUserComponent } from './Pages/create-user/create-user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    
+    },
+
+    {
+        path :'login',
+        component: LoginComponent
+    },
+    {
+        path: '',
+        component: LayoutComponent,
+        children: [
+            {
+                path: 'user-list',
+                component: UserListComponent
+
+             },
+            {
+                path: 'create-user',
+                component: CreateUserComponent
+
+            }
+
+    ]
+    }
+];
